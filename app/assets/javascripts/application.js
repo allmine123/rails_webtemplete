13e614dcca4a5b803722409b0333db48a5ab6a35
allmine123 <s_se123@naver.com>
2018-07-12 05:15:04 +0000

    init, scss, js 분리

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+//= require jquery-1.7.2.min
+//= require html5
+//= require main
+//= require jquery.carouFredSel-6.2.0-packed
+//= require jquery.touchSwipe.min
+//= require checkbox
+//= require radio
+//= require selectBox
+//= require jquery.jqzoom-core
